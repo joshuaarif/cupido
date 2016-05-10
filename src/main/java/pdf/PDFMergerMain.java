3afbff54b27947da36030169254767c66a524333
timpamungkas <timotius.pamungkas@gmail.com>
2016-05-10 18:22:50 +0700

    Add StringTemplate

--- src/main/java/pdf/PDFMergerMain.java
+++ src/main/java/pdf/PDFMergerMain.java
@@ -1,8 +1,17 @@
 package pdf;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadLocalRandom;
+
+import org.apache.commons.io.IOUtils;
+import org.stringtemplate.v4.ST;
+import org.stringtemplate.v4.STGroup;
+import org.stringtemplate.v4.STGroupString;
+import org.stringtemplate.v4.StringRenderer;
+import org.stringtemplate.v4.compiler.CompiledST;
 
 public class PDFMergerMain {
 	// private static final Log LOG = LogFactory.getLog(PDFMergerMain.class);
@@ -15,7 +24,7 @@ public class PDFMergerMain {
 
 		String emailTo = "timotius.pamungkas@gmail.com";
 		String emailSubject = "Email subject ";
-		String emailBody = "Email body";
+		String emailBody = "<strong>Email body</strong>";
 		int size = 3;
 
 		PDFWorkbookGeneratorTask task1 = new PDFWorkbookGeneratorTask(rootWorksheetFolder1, targetFile1, size, emailTo,
@@ -24,9 +33,27 @@ public class PDFMergerMain {
 				emailSubject + ThreadLocalRandom.current().nextInt(), emailBody);
 
 		ExecutorService executorService = Executors.newFixedThreadPool(4);
-		executorService.submit(task1);
-		executorService.submit(task2);
-		
+		// executorService.submit(task1);
+		// executorService.submit(task2);
+
+		InputStream templateStream = PDFMergerMain.class.getClassLoader().getResourceAsStream("email_body.html");
+		try {
+			String myString = IOUtils.toString(templateStream, "UTF-8");
+			STGroup stGroup = new STGroup('$', '$');
+			stGroup.registerRenderer(String.class, new StringRenderer());
+			CompiledST compiledST = stGroup.defineTemplate("temp", myString);
+			compiledST.hasFormalArgs = false;
+
+			ST st = stGroup.getInstanceOf("temp");
+			st.add("name", "Test");
+
+			String x = st.render();
+			System.out.println(x);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		executorService.shutdown();
 	}
 
